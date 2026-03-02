0bfec179b0ab2c397a8735731514c7bd2a5848f8
Pedro Dominguez <pdominguez@betterfly.com>
2026-03-01 22:21:12 -0500

    feat: implemented animation for slider challenge

--- lib/main.dart
+++ lib/main.dart
@@ -4,118 +4,149 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyAppState extends State<MyApp> {
+  final int numDots = 4;
+  int _activeDotIndex = 1;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      setState(() {
+        _activeDotIndex = 0;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(0xFFF6F6F9),
+              borderRadius: BorderRadius.all(Radius.circular(80)),
             ),
-          ],
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ButtomAction(
+                  backgroundColor: Color(0xFFDAD9E4),
+                  icon: Icons.arrow_back_ios_new_outlined,
+                  iconColor: Colors.deepPurple,
+                  action: () {
+                    setState(() {
+                      _activeDotIndex = (_activeDotIndex - 1) % numDots;
+                    });
+                  },
+                ),
+
+                ...List.generate(
+                  numDots,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: IndicatorDots(
+                      isActive: index == _activeDotIndex,
+                      onEnd: () {
+                        setState(() {
+                          _activeDotIndex = (_activeDotIndex + 1) % numDots;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+
+                ButtomAction(
+                  backgroundColor: Color(0xFF2B2B2E),
+                  icon: Icons.arrow_forward_ios_outlined,
+                  iconColor: Colors.white,
+                  action: () {
+                    setState(() {
+                      _activeDotIndex = (_activeDotIndex + 1) % numDots;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class IndicatorDots extends StatelessWidget {
+  const IndicatorDots({super.key, required this.isActive, required this.onEnd});
+
+  final bool isActive;
+  final VoidCallback onEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 900),
+      padding: EdgeInsets.all(5),
+      width: isActive ? 70 : 20,
+      height: 20,
+      curve: Curves.elasticOut,
+      decoration: BoxDecoration(
+        color: Color(0xFFDAD9E4),
+        borderRadius: BorderRadius.all(Radius.circular(80)),
+      ),
+      child: Align(
+        alignment: AlignmentGeometry.centerLeft,
+        child: AnimatedContainer(
+          onEnd: isActive ? onEnd : null,
+          duration: Duration(seconds: 3),
+          width: isActive ? 60 : 10,
+          child: Container(
+            decoration: BoxDecoration(
+              color: isActive ? Colors.white : Colors.transparent,
+              borderRadius: BorderRadius.all(Radius.circular(80)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtomAction extends StatelessWidget {
+  const ButtomAction({
+    super.key,
+    required this.icon,
+    required this.action,
+    required this.iconColor,
+    required this.backgroundColor,
+  });
+
+  final Color backgroundColor;
+  final Color iconColor;
+  final IconData icon;
+  final VoidCallback action;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.all(Radius.circular(80)),
+        ),
+        child: Icon(icon, color: iconColor),
       ),
     );
   }
