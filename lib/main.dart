5b9f2554aa2a8c3c9ec3ece96537176b2b11f9f0
Pedro Dominguez <pdominguez@betterfly.com>
2026-03-01 22:39:58 -0500

    feat: update UI layout and add developer information

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class _MyAppState extends State<MyApp> {
   final int numDots = 4;
   int _activeDotIndex = 1;
 
+  final imageUrl =
+      'https://avatars.githubusercontent.com/u/93799115?s=400&u=c14fd1eee86f09a50970fa9f856cb2e48d17191c&v=4';
+
   @override
   void initState() {
     super.initState();
@@ -30,55 +33,76 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xFFF6F6F9),
-              borderRadius: BorderRadius.all(Radius.circular(80)),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 40,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              child: Image.network(
+                imageUrl,
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ButtomAction(
-                  backgroundColor: Color(0xFFDAD9E4),
-                  icon: Icons.arrow_back_ios_new_outlined,
-                  iconColor: Colors.deepPurple,
-                  action: () {
-                    setState(() {
-                      _activeDotIndex = (_activeDotIndex - 1) % numDots;
-                    });
-                  },
-                ),
+            Text(
+              'Developer: Wachu985',
+              style: TextTheme.of(context).titleLarge,
+            ),
 
-                ...List.generate(
-                  numDots,
-                  (index) => Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: IndicatorDots(
-                      isActive: index == _activeDotIndex,
-                      onEnd: () {
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF6F6F9),
+                  borderRadius: BorderRadius.all(Radius.circular(80)),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ButtomAction(
+                      backgroundColor: Color(0xFFDAD9E4),
+                      icon: Icons.arrow_back_ios_new_outlined,
+                      iconColor: Colors.deepPurple,
+                      action: () {
+                        setState(() {
+                          _activeDotIndex = (_activeDotIndex - 1) % numDots;
+                        });
+                      },
+                    ),
+
+                    ...List.generate(
+                      numDots,
+                      (index) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: IndicatorDots(
+                          isActive: index == _activeDotIndex,
+                          onEnd: () {
+                            setState(() {
+                              _activeDotIndex = (_activeDotIndex + 1) % numDots;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+
+                    ButtomAction(
+                      backgroundColor: Color(0xFF2B2B2E),
+                      icon: Icons.arrow_forward_ios_outlined,
+                      iconColor: Colors.white,
+                      action: () {
                         setState(() {
                           _activeDotIndex = (_activeDotIndex + 1) % numDots;
                         });
                       },
                     ),
-                  ),
+                  ],
                 ),
-
-                ButtomAction(
-                  backgroundColor: Color(0xFF2B2B2E),
-                  icon: Icons.arrow_forward_ios_outlined,
-                  iconColor: Colors.white,
-                  action: () {
-                    setState(() {
-                      _activeDotIndex = (_activeDotIndex + 1) % numDots;
-                    });
-                  },
-                ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
